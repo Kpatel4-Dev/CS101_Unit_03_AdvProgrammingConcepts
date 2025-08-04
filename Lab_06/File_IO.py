3e8d6fa8edd67c74baca967f66f1a2122d1c6bb9
Kavita Patel <kpatel@miracosta.edu>
2025-08-03 20:11:05 -0700

    Create File_IO.py

---
+++ Lab_06/File_IO.py
@@ -0,0 +1,40 @@
+'''
+Lab 06 - Programming in Python: File Input and Output
+
+Description: You are going to learn how to read files, write to files, and you will learn a couple of cool little tricks! Everything we are going to use is built-in for Python and is extremely easy to use!  
+
+Developer Name(s): 
+
+Date: 
+
+'''
+##########################################
+# IMPORTS:
+#   modules needed for program
+##########################################
+# nothing here this lab
+
+
+##########################################
+# FUNCTIONS:
+#   functions useful to main program
+##########################################
+# nothing here this lab
+
+
+##########################################
+# MAIN PROGRAM:
+#   beginning of running program
+##########################################
+
+'''your code for step 1 goes below this statement'''
+
+
+
+
+'''your code for step 2 goes below this statement'''
+
+
+
+
+'''your code for step 3 goes below this statement'''
